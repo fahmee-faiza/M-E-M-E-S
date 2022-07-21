62e7ef726ab5db921d697d2b7808cfe937090074
Fahmee Faiza <53439873+fahmee-faiza@users.noreply.github.com>
2022-07-21 17:26:45 +0600

    Create memes.js

---
+++ memes.js
@@ -0,0 +1,10 @@
+ function changeImage() {
+            var image = document.getElementById('myImage');
+            if (image.src.match("colorbottel")) {
+                image.src = "/memes/a2.jpg";
+            }
+            else {
+                image.src = "/memes/a2.jpg";
+            }
+           
+        }
